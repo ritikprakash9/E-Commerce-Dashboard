a426b4bad5317a3b6396ab03c9485a69e3ef65e6
Ritik Prakash <prakashritik97@gmail.com>
2024-01-26 10:55:20 +0530

    connect mongodb database

--- E-Dashboard/Backend/index.js
+++ E-Dashboard/Backend/index.js
@@ -2,9 +2,22 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res)=>{
-    res.send("App is working...");
-})
+// => Mongo DB Connection 
+
+const mongooes = require('mongoose');
+
+const connectDB = async()=>{
+    mongooes.connect('mongodb://localhost:27017/College');
+    const studentSchema = new mongooes.Schema({});
+    const student = mongooes.model('Student', studentSchema);
+    const data = student.find();
+    console.warn(data);
+}
+
+connectDB();
+// app.get('/', (req, res)=>{
+//     res.send("App is working...");
+// })
 
 
-app.listen(5000);
+// app.listen(5000);
